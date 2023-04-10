c6907dca00545099b0c73fff2b05f6cbf2fe3948
André <andre.l10@aluno.ifsc.edu.br>
2023-04-10 20:35:04 -0300

    criação dos arquivos servidor.js e rotas.js

---
+++ servidor.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+
+server.listen(3000, ()=>{
+    console.log("Servidor em execução... ")
+})
